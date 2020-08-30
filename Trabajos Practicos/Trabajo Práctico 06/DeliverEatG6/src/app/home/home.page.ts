5e8a6c5293e0d08f6162c98ebde47ce72363650c
MatiasMolina <maty.f.molina4a@gmail.com>
2020-08-30 01:42:16 -0300

    add:Se empezo a diseñar la interfaz de inicio

--- Trabajos Practicos/Trabajo Práctico 06/DeliverEatG6/src/app/home/home.page.ts
+++ Trabajos Practicos/Trabajo Práctico 06/DeliverEatG6/src/app/home/home.page.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-
+import {HomeService} from '../services/home.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  comercio
+  constructor(private homeService:HomeService) {}
 
-  constructor() {}
-
+  ngOnInit() {
+    this.comercio = this.homeService.getComercios();
+    console.log(this.comercio)
+  }
 }
